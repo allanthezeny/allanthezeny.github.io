393a8d42971d85c9073f56f63347ee5fc9a39229
allanthezeny <allanpriston@gmail.com>
2018-09-03 16:44:17 -0300

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  $("#age").val(calcularidade());
   $(".jump").on("click", function( e ) {
 
     e.preventDefault();
@@ -10,3 +11,17 @@ $(document).ready(function() {
   });
 
 });
+
+
+
+function calcularidade() {
+  var dob = new Date("1993/01/11");
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  var birthdayThisYear = new Date(currentYear, dob.getMonth(), dob.getDate());
+  var age = currentYear - dob.getFullYear();
+  if(birthdayThisYear > currentDate) {
+    age--;
+  }
+  return age;
+}
